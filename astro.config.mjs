529cfbe47b7b0509818c307d964fd67f66133579
David Lapsley <david@actualyze.ai>
2026-01-11 11:48:10 -0500

    chore: add gh pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://kodeops.dev',
+  site: 'https://kodeops-ai.github.io',
+  base: '/kodeops-site',
   integrations: [
     tailwind(),
     sitemap()
